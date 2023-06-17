459bebf8c7db44e1cf9d388fa50e2f72b0d922cc
Andres Camperos <88594759+FractureDVL@users.noreply.github.com>
2023-06-17 06:17:26 -0500

    Update AxiosInstance.tsx

--- src/utils/AxiosInstance.tsx
+++ src/utils/AxiosInstance.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8081', // Ruta base de tu API
+    //baseURL: 'http://localhost:8081'
+    baseURL: 'https://softtictac.azurewebsites.net', // Ruta base de tu API
     headers: {
         // Opciones de encabezado comunes, si es necesario
         'Content-Type': 'application/json',
